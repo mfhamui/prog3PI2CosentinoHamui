4c9c36c648c7b9bb7aadd5a96a7973a256c385a7
mfhamui <mfhamui@gmail.com>
2025-11-03 17:44:05 -0300

    nuevoost

--- PI2/src/components/menu.js
+++ PI2/src/components/menu.js
@@ -2,8 +2,8 @@ import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import AntDesign from '@expo/vector-icons/AntDesign';
-
-import home from "../screens/home";
+import  home  from "../screens/home";
+import nuevoPost from "../screens/nuevoPost";
 
 
 const Tab = createBottomTabNavigator();
@@ -15,7 +15,8 @@ function menu(params) {
             <Tab.Screen name="home" component={home} options={
                 { tabBarIcon: () => <FontAwesome name="home" size={24} color="black" /> }
             } />
-           
+           <Tab.Screen name="nuevoPost" component={ nuevoPost } options={ 
+	            { tabBarIcon: () =>  <AntDesign name="plus" size={24} color="black" />  }} />
         </Tab.Navigator>
 
     )
