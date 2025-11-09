675a549689c9eea5ccadb083c6b1fc986d0b80c0
mfhamui <mfhamui@gmail.com>
2025-11-09 16:01:42 -0300

    css

--- PI2/src/screens/Home.js
+++ PI2/src/screens/Home.js
@@ -62,9 +62,10 @@ const style = StyleSheet.create({
 
   container: {
     flex: 1,
-    paddingHorizontal: 10,
-    marginTop: 20,
-    width: "100%"
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    backgroundColor: "#ffffff",
+   
   },
 
   titulo: {
@@ -72,6 +73,7 @@ const style = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 10,
   },
+ 
 
 })
 
