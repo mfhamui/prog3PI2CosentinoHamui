cac845c2c140f22db8972c6e89f3593f3b52bad8
mfhamui <mfhamui@gmail.com>
2025-11-10 15:33:13 -0300

    .

--- PI2/src/components/Post.js
+++ PI2/src/components/Post.js
@@ -65,7 +65,7 @@ class Post extends Component {
           <Pressable onPress={() => this.likearPost()}>
             <Text style={styles.gustar}> {this.state.texto ? "Dar me gusta" : "Eliminar me gusta"}</Text>
           </Pressable>
-          <Pressable style={styles.comentario} onPress={() => this.props.navigation.navigate('Comentar', { comentarios: this.props.comentarios, likes: this.props.likes,})}>
+          <Pressable style={styles.comentario} onPress={() => this.props.navigation.navigate('Comentar', { comentarios: this.props.comentarios, likes: this.props.likes, email:this.props.email})}>
             <Text > Comentar </Text>
           </Pressable>
 
